2fb65edcb7d31b52e10e8c6e274d70c70904d330
Maksim Ryzhikov <rv.maksim@gmail.com>
2016-12-17 16:05:38 +0300

    fix: load only test file

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,7 @@ module.exports = function(config) {
         basePath: '',
         frameworks: ['jasmine'],
         files: [
-            "./*"
+            "./*.spec.js"
         ],
         exclude: [
             "./node_modules"
